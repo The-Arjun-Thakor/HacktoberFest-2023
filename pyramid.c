07e441acd9377a89770e8f3fbebda759fc6e488e
Aditya <121703339+aditya-jnu@users.noreply.github.com>
2023-10-21 01:46:18 +0530

    Create pyramid.c

---
+++ pyramid.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,j;
+    printf("Enter the no. of line\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=2*n-1;j++)
+        {
+            if(j>=n-(i-1)&&j<=n+(i-1))
+                printf("*");
+              else
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
